15303e0f31f4bb51e4b065113a39b1afdae1969b
ifeanyimuogbo <doc3coder@gmail.com>
2021-01-25 18:48:51 +0100

    fix self signed cert error

--- src/models/index.js
+++ src/models/index.js
@@ -6,7 +6,12 @@ if (process.env.DATABASE_URL) {
   sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     ssl: true,
-    dialectOptions: { ssl: true },
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   });
 } else {
   sequelize = new Sequelize(
